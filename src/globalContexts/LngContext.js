30530ab1fdba75fefd7020728bfa4549da0f2332
Nurbek <nurbekizbassar@gmail.com>
2019-07-12 22:56:44 +0600

    make default language

--- src/globalContexts/LngContext.js
+++ src/globalContexts/LngContext.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
+import {DEFAULT_LOCALE} from '../config/constants';
 
-export const LngContext = React.createContext(['ru']);
+export const LngContext = React.createContext([DEFAULT_LOCALE]);
 
 export default function LngContextProvider({ children }) {
-  const [lng, setLng] = useState('ru');
+  const [lng, setLng] = useState(DEFAULT_LOCALE);
   return (
     <LngContext.Provider value={[lng, setLng]}>{children}</LngContext.Provider>
   );
